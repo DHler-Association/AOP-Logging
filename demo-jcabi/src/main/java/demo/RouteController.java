a9dc120c99de20e5cf703f6bec4e897fe7f04c97
MarkoHaupt <101417551+MarkoHaupt@users.noreply.github.com>
2022-12-14 14:51:21 +0100

    Add files via upload

---
+++ demo-jcabi/src/main/java/demo/RouteController.java
@@ -0,0 +1,41 @@
+package demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.jcabi.aspects.LogExceptions;
+import com.jcabi.aspects.Loggable;
+
+@Loggable
+@RestController
+public class RouteController {
+
+	//Hello Route
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/hello"
+    )
+    public ResponseEntity<?> getHello(
+            @RequestParam("name") String username
+    ) {
+        return ResponseEntity.ok("Hello, " + username);
+    }
+    
+    
+    //add Route
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/add"
+    )
+    public ResponseEntity<?> add(
+            @RequestParam("n1") int number1,
+            @RequestParam("n2") int number2
+    ) throws InterruptedException {
+    	int result = number1 + number2;
+    	
+    	Thread.sleep(50);
+        
+    	return ResponseEntity.ok(number1 + " + " + number2 + " = " + result);
+    }
+
+}
