3b63c86c2543eea83999d4c68c39c26762f6decf
MarkoHaupt <101417551+MarkoHaupt@users.noreply.github.com>
2023-01-18 13:31:48 +0100

    Add files via upload

---
+++ demo-jcabi/src/main/java/de/svi/demo/RouteController.java
@@ -0,0 +1,36 @@
+package de.svi.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.jcabi.aspects.Loggable;
+
+@RestController
+public class RouteController {
+
+	//Simple Hello World Route
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/"
+    )
+    @Loggable
+    public ResponseEntity<?> getHelloWorld() {
+        return ResponseEntity.ok("Hello World");
+    }
+    
+    //add Route with parameters
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/add"
+    )
+    @Loggable
+    public ResponseEntity<?> add(
+            @RequestParam("n1") int number1,
+            @RequestParam("n2") int number2
+    ) throws InterruptedException {
+    	int result = number1 + number2;
+        
+    	return ResponseEntity.ok(number1 + " + " + number2 + " = " + result);
+    }
+	
+}
